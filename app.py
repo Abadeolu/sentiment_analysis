9a2014698ebcb62f4e6066cc3ef4d612fec6efaf
Abraham Yawson <abadeolu@gmail.com>
2023-10-10 00:46:44 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+app = Flask(__name__)
+
+analyser = SentimentIntensityAnalyzer()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analyse_sentiment', methods=['POST'])
+def analyse_sentiment():
+    input_text = request.get_json()['input']
+    sentiment = sentiment_analyzer_scores(input_text)
+    return jsonify({'sentiment': sentiment})
+
+def sentiment_analyzer_scores(sentence):
+    score = analyser.polarity_scores(sentence)
+    # Determine the sentiment label based on the compound score
+    if score['compound'] >= 0.05:
+        sentiment_label = 'Positive'
+    elif score['compound'] <= -0.05:
+        sentiment_label = 'Negative'
+    else:
+        sentiment_label = 'Neutral'
+    return sentiment_label
+
+if __name__ == '__main__':
+    app.run(debug=True)
